75370cec761501ce252cb3e9abb62fba79230eee
amtp1 <magomedov.abdul20012@gmail.com>
2022-03-09 22:06:55 +0300

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from pathlib import Path
-from datetime import datetime as dt
 
-import pandas as pd
-from binance.spot import Spot
 import yaml
-
 from loguru import logger
-from time import sleep
 
 from utils.utils import *
 from exceptions.exceptions import *
@@ -19,40 +14,12 @@ def main():
         config = yaml.load(f, Loader=yaml.FullLoader)
 
     logger.add(
-        r"debug/debug.log", format="{time} {level} {message}",
+        r"debug/debug_check_profit.log", format="{time} {level} {message}",
         level="DEBUG", rotation="1 week",
         compression="zip")
 
     driver = Driver(config)
-
-    params = {
-        'symbol': 'DOTRUB',
-        'side': "BUY",
-        'type': 'MARKET',
-        'quantity': 1.3,
-    }
-
-    #print(driver.get_price_by_symbol(symbol="DOTRUB"))
-    #response = driver.create_new_order(**params)
-    #logger.info(response)
     driver.stream()
-
-    """
-    with open(f"orders_100.txt", "w") as f:
-        for i in range(0, 100):
-            if i % 2 == 0:
-                params["side"] = "SELL"
-            else:
-                params["side"] = "BUY"
-            response = driver.create_new_order(**params)
-            f.write(str(response)+"\n")
-        f.close()
-    """
-
-    #print(driver.depth(symbol="DOTRUB", limit=5))
-    #print(driver.my_trades(symbol="DOTUSDT"))
-    #fee = driver.trade_fee(symbol="DOTUSDT")
-    #print(fee)
 
 if __name__ == "__main__":
     try:
