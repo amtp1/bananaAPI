948d5002b77df48b9d0f2ae61968184c862bcbd2
amtp1 <magomedov.abdul20012@gmail.com>
2022-03-10 12:15:22 +0300

    refactor code and to add commentaries

--- utils/utils.py
+++ utils/utils.py
@@ -1,101 +1,198 @@
 from math import remainder
 from time import sleep
 from datetime import datetime as dt
-from decimal import Decimal as D, ROUND_DOWN, ROUND_UP
 from itertools import count
 
 from binance.spot import Spot
 import pandas as pd
 from loguru import logger
 
+from .formatting import *
 from exceptions.exceptions import *
+
 
 class Driver:
     def __init__(self, config: dict):
-        self.client = Spot(key=config.get("API-Key"), secret=config.get("Secret-Key"))
+        self.client = Spot(key=config.get("API-Key"), secret=config.get("Secret-Key")) # Init client object with configs.
         self.steps_symbols = [
             {"couple": ["DOT", "RUB"], "side": "BUY"},
             {"couple": ["DOT", "USDT"], "side": "SELL"},
             {"couple": ["USDT", "RUB"], "side": "SELL"}
-        ]
-        self.remainder = 50 # Remainder balance. Default: roubles.
-        self.percent = 0.1 # Percent value
+        ] # This is main steps for create orders.
+        self.remainder = 50 # Remainder balance. Default is roubles.
+        self.percent = 0.1 # Percent value.
 
     def current_balance(self) -> float:
-        balance: float = 0.0
+        """The function takes all value of coins from client balance and return generated balance
+        in the dollar value.
+
+        Params
+        ------
+            Non
+
+        Returns
+        -------
+            balance : float
+                Current balance of client.
+        """
+
+        balance: float = 0.0 # Init balance variable with value. Default is 0.0
+
         for coin in self.client.account().get("balances"):
-            asset, free, locked = coin.values()
+            asset, free, locked = coin.values() # Init asset and free variables.
+
+            # Check free for quantity.
             if float(free) > 0:
+                # Check asset. Must not be in rub or usdt value if it another coins.
                 if not asset in ("RUB", "USDT",):
                     try:
-                        price: float = float(self.depth(symbol=f"{asset}USDT", limit=1).get("bids")[0][0])
-                        dollar_value: float = float(free) * price
-                        balance+=dollar_value
+                        price: float = float(self.depth(symbol=f"{asset}USDT", limit=1).get("bids")[0][0]) # Get price from api.
+                        qty: float = float(free) * price # Generated qty from coins in the dollar value.
+                        balance+=qty # Add the quantity to the balance value.
                     except:
                         pass
                 elif asset == "USDT":
-                    balance+=float(free)
+                    balance+=float(free) # Add the quantity to the balance value. Default is dollar value.
         return balance
 
-    def get_value_by_symbol(self, symbol: str) -> float:
-        value: float = float([coin.get("free") for coin in self.client.account().get("balances") if symbol == coin.get("asset")][0])
-        return value
+    def get_qty_by_symbol(self, symbol: str) -> float:
+        """The function takes quantity by symbol from balance and checking for symbol.\n
+        If symbol equals asset then return quantity.
 
-    def create_new_order(self, **kwargs):
+        Params
+        ------
+            symbol : str
+                The symbol of coin.
+
+        Returns
+        -------
+            qty : float
+                Current quantity of coin.
+        """
+
+        qty: float = float([coin.get("free") for coin in self.client.account().get("balances") if symbol == coin.get("asset")][0])
+        return qty
+
+    def create_new_order(self, **kwargs) -> dict:
+        """The function create new order.
+
+        Params
+        ------
+            kwargs : dict
+                The kwargs structure consists of symbol, side, type, quantity.
+                Default values is None, None, Market, None.
+
+        Returns
+        -------
+            response : dict
+                The main response with all values from api.
+        """
+
         try:
-            response = self.client.new_order(**kwargs)
+            response = self.client.new_order(**kwargs) # Api response.
             return response
         except Exception as e:
             raise OrderError(e)
 
-    def depth(self, save_to_exel: bool = None, symbol: str = None, limit: int = None) -> dict:
+    def depth(self, save_to_exel: bool = False, symbol: str = None, limit: int = None) -> dict:
+        """The function takes depths by symbol and limit value.
+
+        Params
+        ------
+            save_to_excel : bool
+                If you need to save the depths in an excel file.
+            symbol : str
+                Default is None. Symbol of coin.
+            limit : int
+                Default is None. Quantity of limit depths.
+
+        Returns
+        -------
+            depths : dict
+                All the depths.
+        """
+
         if not limit:
             raise DepthError("limit value is empty")
         elif not symbol:
             raise DepthError("symbol value is empty")
 
         try:
-            last_orders = self.client.depth(symbol=symbol, limit=limit)
+            depths = self.client.depth(symbol=symbol, limit=limit) # Get all depths from API.
             if save_to_exel:
-                df = pd.DataFrame(last_orders)
-                df.to_excel("orders.xlsx")
-            return last_orders
+                df = pd.DataFrame(depths)
+                df.to_excel("depths.xlsx") # Save depths.
+            return depths
         except Exception as e:
             raise DepthError(e)
 
-    def my_trades(self, symbol: str = None):
-        # time = dt.fromtimestamp(int(str(trade["time"])[:-3])).strftime("%Y-%m-%d %H:%M:%S")
+    def my_trades(self, symbol: str = None) -> list:
+        """The function takes all client trades.
+        Time format: dt.fromtimestamp(int(str(trade["time"])[:-3])).strftime("%Y-%m-%d %H:%M:%S")
+
+        Params
+        ------
+            symbol : str
+                Default is None. Symbol of coin.
+
+        Returns
+        -------
+            trades : list
+                The all trades by symbol from client.
+        """
+
         try:
-            trades = self.client.my_trades(symbol=symbol)
+            trades = self.client.my_trades(symbol=symbol) # Get all trades from client by symbol.
             return trades
         except Exception as e:
             raise TradesError(e)
 
-    def exchange_info(self, symbol: str = None):
-        if not symbol:
-            raise ExchangeError("symbol value is empty")
+    def trade_fee(self, symbol: str = None) -> list:
+        """The function takes trade fee by symbol.
 
-        exchng_info = self.client.exchange_info(symbol=symbol)
-        return exchng_info
+        Params
+        ------
+            symbol : str
+                Default is None. Symbol of coin.
 
-    def trade_fee(self, symbol: str = None):
+        Returns
+        -------
+            fee : list
+                The trade fee. May generated the order percent.
+        """
+
         if not symbol:
             raise TradesError("symbol value is empty")
 
-        fee = self.client.trade_fee(symbol=symbol)
+        fee = self.client.trade_fee(symbol=symbol) # Get tarde fee.
         return fee
 
     def get_price_by_symbol(self, symbol: str = None, limit: int = 3) -> float:
-        value: float = 0.0
+        """The function get price by symbol.
+
+        Params
+        ------
+            symbol : str
+                Default is None. Symbol of coin.
+            limit : int
+                Quantity prices from orderbook.
+
+        Returns
+        -------
+            price : float
+                Return arithmetic mean price.
+        """
+
+        price: float = 0.0
         bids = self.depth(symbol=symbol, limit=limit).get("bids")
         for n in bids:
-            value+=float(n[0])
-        return float("{:0.2f}".format(value/limit))
+            price+=float(n[0]) # Add price of coin. Different value with each position.
+        return to_float_format(price/limit)
 
     def stream(self):
         get_price = self.get_price_by_symbol # (Function object) Gets the price of a symbol.
-        get_value = self.get_value_by_symbol # (Function object) Gets the balance value of a symbol.
-        to_float = self.to_float_format      # (Function object) Convert value to float type.
+        get_qty = self.get_qty_by_symbol     # (Function object) Gets the balance value of a symbol.
+        to_float = to_float_format           # (Function object) Convert value to float type.
         percent = self.get_percent           # (Function object) Get percent.
 
         params = {
@@ -105,14 +202,14 @@ class Driver:
             "quantity": None
         }
 
-        count_iter: int = 0
-        before_commission: float = 0.0
-        after_commission: float = 0.0
-        remain: float = 0.0
-        start_time = dt.now()
+        count_iter: int = 0 # Count iteration steps. Before cycle value eqauls is 0
+        before_commission: float = 0.0 # Commission value before cycle. Value equals is 0
+        after_commission: float = 0.0 # Commission value after cycle. Value equals is 0
+        start_time = dt.now() # Start time.
+
         while True:
             is_rouble_step = True # Rouble step while iterating. Default: True.
-            roubles = get_value(symbol="RUB") - self.remainder # Roubles balance without remainder.
+            roubles = get_qty(symbol="RUB") - self.remainder # Roubles balance without remainder.
             logger.info(F"Начальный баланс: {to_float(roubles)}₽") # Show balance value with remainder.
 
             for step in self.steps_symbols:
@@ -124,72 +221,73 @@ class Driver:
 
                 if not couple[0] in ("USDT", "RUB",):
                     if is_rouble_step:
-                        #usdt_value = get_value(symbol="USDT")
-                        #if usdt_value > 1:
-                        #    remain = (usdt_value // 1) * get_price(symbol="USDTRUB")
-                        #roubles = roubles + remain
                         before_count_exchng = to_float(roubles / get_price(symbol=symbol))
                         before_commission+=to_float(percent((before_count_exchng * 0.1 / 100) * get_price("DOTUSDT"), is_dollar=True))
                         params["quantity"] = before_count_exchng
                         is_rouble_step=False
-                        response = self.create_new_order(**params)
-                        after_deal = response.get("fills")[0]
-                        after_count_exchng = before_count_exchng
-                        after_commission+=to_float(percent(float(after_deal.get("commission")) * get_price("DOTUSDT"), is_dollar=True))
-                        logger.info(F"1 ITER: {response}")
+                        #response = self.create_new_order(**params)
+                        #after_deal = response.get("fills")[0]
+                        #after_count_exchng = before_count_exchng
+                        #after_commission+=to_float(percent(float(after_deal.get("commission")) * get_price("DOTUSDT"), is_dollar=True))
+                        #logger.info(F"1 ITER: {response}")
                     else:
                         params["quantity"] = before_count_exchng
                         before_count_exchng = to_float(before_count_exchng * get_price(symbol="DOTUSDT")) #+ (get_value(symbol="USDT") // 1)
                         before_commission+=to_float(percent(before_count_exchng * 0.1 / 100, is_dollar=True))
-                        response = self.create_new_order(**params)
-                        after_deal = response.get("fills")[0]
-                        after_count_exchng = before_count_exchng
-                        after_commission+=to_float(percent(float(after_deal.get("commission")), is_dollar=True))
-                        logger.info(F"2 ITER: {response}")
+                        #response = self.create_new_order(**params)
+                        #after_deal = response.get("fills")[0]
+                        #after_count_exchng = before_count_exchng
+                        #after_commission+=to_float(percent(float(after_deal.get("commission")), is_dollar=True))
+                        #logger.info(F"2 ITER: {response}")
                 elif couple[0] == "USDT":
-                    params["quantity"] = self.to_integer_format(before_count_exchng)
+                    params["quantity"] = to_integer_format(before_count_exchng)
                     before_count_exchng = before_count_exchng * to_float(get_price(symbol=symbol))
-                    before_commission+=to_float(percent(value=before_count_exchng, is_rouble=True))
-                    response = self.create_new_order(**params)
-                    after_deal = response.get("fills")[0]
-                    after_count_exchng = before_count_exchng
-                    after_commission+=to_float(percent(float(after_deal.get("commission")), is_rouble=True))
-                    logger.info(F"3 ITER: {response}")
+                    before_commission+=to_float(percent(value=before_count_exchng))
+                    #response = self.create_new_order(**params)
+                    #after_deal = response.get("fills")[0]
+                    #after_count_exchng = before_count_exchng
+                    #after_commission+=to_float(percent(float(after_deal.get("commission")), is_rouble=True))
+                    #logger.info(F"3 ITER: {response}")
                     is_rouble_step = True
 
             before_profit = "{:0.5f}".format(((before_count_exchng - before_commission) * 100 / roubles) - 100)
-            after_profit = "{:0.5f}".format(((after_count_exchng - after_commission) * 100 / roubles) - 100)
+            #after_profit = "{:0.5f}".format(((after_count_exchng - after_commission) * 100 / roubles) - 100)
             logger.info((
-                #F"Конечный баланс: {count_exchng - before_commission}₽\n"
-                #F"Комиссия: {to_float(before_commission)}₽\n"
+                F"Конечный баланс: {before_count_exchng - before_commission}₽\n"
+                F"Комиссия: {to_float(before_commission)}₽\n"
                 F"\nПотенциальный профит: {before_profit}%\n"
-                F"Фактический профит: {after_profit}%\n"
+                #F"Фактический профит: {after_profit}%\n"
                 )
             )
             before_commission = 0.0
             after_commission = 0.0
 
-            end_time = int((dt.now() - start_time).total_seconds())
-
-            #sleep(3)
-
-            #if end_time >= 60:
-            #    break
+            end_time = int((dt.now() - start_time).total_seconds()) # End time.
+            logger.info(F"Time (seconds): {end_time}")
 
             count_iter+=1
-            sleep(3)
             if count_iter == 1:
                 break
 
-    def to_float_format(self, price: str) -> float:
-        return float("{:0.2f}".format(price))
+    def get_percent(self, qty: str = None, is_dollar: bool = False) -> float:
+        """The function generates a percent per order. The percent value is 0.1
 
-    def to_integer_format(self, price: str) -> int:
-        return int(float("{:0.2f}".format(price)))
-    
-    def get_percent(self, value: str = None, is_dollar: bool = False, is_rouble: bool = False) -> float:
+        Params
+        ------
+            qty : str
+                The quantity of coins upcoming from the api. Default is None.
+
+            is_dollar : bool
+                Checking qty for dollar value. Default is False.
+
+        Returns
+        -------
+            qty : float
+                The quantity of coins upcoming from the api but converted to float type.
+        """
+
         if is_dollar:
-            value: float = value * self.get_price_by_symbol(symbol="USDTRUB")
-        elif is_rouble:
-            value: float = value * self.percent / 100
-        return value
+            qty: float = qty * self.get_price_by_symbol(symbol="USDTRUB")
+        else:
+            qty: float = qty * self.percent / 100
+        return qty
