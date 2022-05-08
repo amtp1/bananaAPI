619df60b1c3bb1a11608b0af37906a4adaf4c2f6
anonymous <info@1inch.io>
2022-05-08 14:37:19 +0300

    refactoring code, add new constants, add argparse

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
-from pathlib import Path
-from datetime import datetime as dt
+from os import path, mkdir
+import argparse
 
-import yaml
-from loguru import logger
-
+from logger.logger import logger
 from utils.utils import *
 from exceptions.exceptions import *
 
+BDATA_PATH = r"bdata"
+DEBUG_PATH = r"debug"
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 def main():
-    if not Path(r"config.yaml").is_file():
-        raise FileError("'config.yaml' is not found")
+    # Check bdata folder.
+    if not path.exists(BDATA_PATH):
+        mkdir(BDATA_PATH) # Create bdata folder.
+    # Check debug folder.
+    elif not path.exists(DEBUG_PATH):
+        mkdir(DEBUG_PATH) # Create debug folder.
 
-    with open(r"config.yaml") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
+    parser = argparse.ArgumentParser(description="BBC (Base Banana Commands)") # Init Argument Parser object
+    parser.add_argument('-current_balance', action='store_true', help='Show current balance of account') # Set argument for run base func
+    parser.add_argument('-trades', type=str, default=None, help='Show trades of account. Example: python main.py -trades=USDTRUB') # Set argument for run base func
+    parser.add_argument('-to_excel', type=str2bool, default=False, help='Save trades to excel file') # Save trades to excel file
+    parser.add_argument('-run', action='store_true', help="Run stream") # Run stream (main function)
+    parser.add_argument('-iter', type=int, default=0, help="Count iterations") # Count iterations
 
-    n_dt = dt.now().strftime("%Y-%m-%d")
-    logger.add(
-        r"debug/debug_%s.log" % n_dt, format="{time} {level} {message}",
-        level="DEBUG", rotation="1 week",
-        compression="zip")
-
-    driver = Driver(config)
-    driver.stream()
+    args = parser.parse_args() # Parse arguments
+    if not any(args.__dict__.values()):
+        parser.print_help() # Show help message
+    else:
+        core = Core() # Init Core object
+        if args.current_balance:
+            current_balance = core.current_balance() # Get current balance
+            logger.info("Current balance: {:.2f}$".format(current_balance)) # Show current balance
+        elif args.trades:
+            core.trades(symbol=args.trades, to_excel=args.to_excel)
+        elif args.run:
+            if args.iter <= 0:
+                return logger.info("Wrong value. Must not be less than or equal to zero.")
+            else:
+                core.stream(count_iter=args.iter)
 
 if __name__ == "__main__":
     try:
-        main()
+        main() # Call main function
     except KeyboardInterrupt:
         logger.info("Program stopped.")
