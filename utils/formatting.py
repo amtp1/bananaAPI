21ef08738e9a413bb4290b6e35bce0ab52a11427
anonymous <info@1inch.io>
2022-05-08 14:38:20 +0300

    check valid symbol

--- utils/formatting.py
+++ utils/formatting.py
@@ -12,7 +12,7 @@ def to_float_format(qty: str) -> float:
             The quantity of coins upcoming from the api but converted to float type.
     """
 
-    return float("{:0.2f}".format(qty))
+    return float("{:0.2f}".format(qty)) # Return correct int format
 
 def to_integer_format(qty: str) -> int:
     """The function converting qty to integer type.
@@ -28,4 +28,11 @@ def to_integer_format(qty: str) -> int:
             The quantity of coins upcoming from the api but converted to integer type.
     """
 
-    return int(float("{:0.2f}".format(qty)))
+    return int(float("{:0.2f}".format(qty))) # Return correct int format
+
+def valid_symbol(symbol: str) -> str:
+    BLOCKED_SYMBOL_PART = ("nft",) # Blocked parts (is not valid)
+
+    # Check symbol for valid
+    if not symbol.lower() in BLOCKED_SYMBOL_PART:
+        return F"{symbol}USDT" # Return correct format
